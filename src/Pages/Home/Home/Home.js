15de550e4d179aa20f4f2862f7c67262ca3759ae
nesaranaeem <hello@nesaran.com>
2022-11-13 03:08:16 +0600

    initialization

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import InfoCards from "../Cards/InfoCards";
+import Hero from "../Hero/Hero";
+
+const Home = () => {
+  return (
+    <>
+      <Hero></Hero>
+      <InfoCards></InfoCards>
+    </>
+  );
+};
+
+export default Home;
