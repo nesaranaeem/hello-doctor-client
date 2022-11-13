3e4e7954fcd2814f9dd852f3267b174f19c3a05f
nesaranaeem <hello@nesaran.com>
2022-11-13 15:09:22 +0600

    hero section added

--- src/Pages/Home/Cards/InfoCard.js
+++ src/Pages/Home/Cards/InfoCard.js
@@ -3,9 +3,9 @@ import React from "react";
 const InfoCard = ({ card }) => {
   const { id, name, description, icon, bgClass } = card;
   return (
-    <div className={`card p-6 card-side ${bgClass} shadow-xl`}>
+    <div className={`card p-6 md:card-side ${bgClass} shadow-xl`}>
       <figure>
-        <img src={icon} alt="Movie" />
+        <img src={icon} alt="" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -1,23 +1,31 @@
 import React from "react";
 import chair from "../../../assets/images/chair.png";
+import heroBg from "../../../assets/images/bg.png";
+import PrimaryButton from "../../../Components/Buttons/PrimaryButton";
 const Hero = () => {
+  const heroTitle = "Your New Smile Starts Here";
+  const heroDescription =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the";
   return (
-    <div className="hero">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} className="rounded-lg w-1/2 shadow-2xl" alt="" />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn text-white bg-gradient-to-r from-primary to-secondary">
-            Get Started
-          </button>
+    <section className="my-5">
+      <div
+        className="hero"
+        style={{
+          background: `url(${heroBg})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPositionY: "center",
+        }}
+      >
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={chair} className="rounded-lg w-1/2 shadow-2xl" alt="" />
+          <div>
+            <h1 className="text-5xl font-bold">{heroTitle}</h1>
+            <p className="py-6">{heroDescription}</p>
+            <PrimaryButton>Get Started</PrimaryButton>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
